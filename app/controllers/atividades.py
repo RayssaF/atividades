c8ce845d94912e6e2593ef6d2951ddd35a541a88
RayssaF <yssa12999gabriela@gmail.com>
2020-03-10 20:38:35 -0400

    aula 4, formulario de atividades

--- app/controllers/atividades.py
+++ app/controllers/atividades.py
@@ -1,5 +1,8 @@
 from app import app
 from flask import render_template
+from flask import request
+import uuid
+import os
 from app.models.tables import Atividade
 
 #listando todas as atividades
@@ -11,3 +14,18 @@ def listar_atividades():
         a[i].data = a[i].data.strftime('%d/%m/%Y')
 
     return render_template("atividades_listar.html", atividades=a)
+
+#Carregando formulário
+@app.route('/atividades/novo')
+def form_nova_atividade():
+    tipo = 'inserir'
+    return render_template("atividades_formulario.html", tipo=tipo)
+
+# Cadastrar uma nova atividade
+@app.route('/atividades/novo', methods=['POST'])
+def inserir_atividades():
+    # Recebendo arquivo
+    arquivo = request.files['arquivo']
+    arquivo.filename = str(uuid.uuid4()) + os.path.splitext(arquivo.filename)[1]
+    print(arquivo.filename)
+    return ""
